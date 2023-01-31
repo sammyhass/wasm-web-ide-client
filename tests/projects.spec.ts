5c0ea8d6eaa607735b783296ade7cd7287b4febf
Sammy Hass <sammyhass01@gmail.com>
2023-01-31 11:13:22 +0000

    refac waiting for project page

--- tests/projects.spec.ts
+++ tests/projects.spec.ts
@@ -1,4 +1,4 @@
-import test, { expect } from '@playwright/test';
+import test, { expect, Page } from '@playwright/test';
 import { LoginRegisterPage } from './pom/LoginRegisterPage';
 import { Navbar } from './pom/Navbar';
 import { createRandomProjectName, NewProjectPage } from './pom/NewProjectPage';
@@ -36,14 +36,44 @@ test('can create new project', async ({ page }) => {
   });
 
   await test.step('expect redirect to project page with correct title in navbar', async () => {
-    const regex =
-      /\/projects\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/;
-
-    await page.waitForURL(url => regex.test(url.href));
-    await page.waitForLoadState('networkidle');
+    await waitForProjectPage(page, projectName);
 
     const navbar = new Navbar(page);
 
     expect(await navbar.title.innerText()).toContain(projectName);
   });
+
+  await test.step('can see project in projects page', async () => {
+    await page.goto('/projects');
+    await page.waitForLoadState('networkidle');
+    expect(
+      await projectsPage.projectCards
+        .getByTestId('project-title')
+        .allInnerTexts()
+    ).toContain(projectName);
+  });
+
+  await test.step('click on project in projects page to go to project page', async () => {
+    await projectsPage.projectCards.getByText(projectName).click();
+
+    await waitForProjectPage(page, projectName);
+
+    const navbar = new Navbar(page);
+    expect(await navbar.title.innerText()).toContain(projectName);
+  });
+
+  // TODO: implement delete project test
+  // await test.step('can delete project', async () => {
+  // });
 });
+
+async function waitForProjectPage(page: Page, title: string) {
+  const navbar = new Navbar(page);
+  const regex =
+    /\/projects\/[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/;
+
+  await page.waitForURL(url => regex.test(url.href));
+  await page.waitForLoadState('networkidle');
+
+  await navbar.title.locator(`text=${title}`).waitFor();
+}
