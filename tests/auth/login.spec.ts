39a1e934363e5a0ac5b6a75825f4a2a99033b232
Sammy Hass <sammyhass01@gmail.com>
2023-04-18 22:48:45 +0100

    fix: flakiness

--- tests/auth/login.spec.ts
+++ tests/auth/login.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from '@playwright/test';
 import { getTestUser, getURL } from '../util';
 import {
-  createRandomLoginInput,
   LoginRegisterPage,
+  createRandomLoginInput,
 } from '../util/pom/LoginRegisterPage';
 import { Navbar } from '../util/pom/Navbar';
 
@@ -14,13 +14,13 @@ test('can login', async ({ page }) => {
 
   await pom.login(testUser.email, testUser.password);
 
-  await page.waitForLoadState('networkidle');
   await page.waitForURL(getURL('/projects'));
   expect(page.url()).toBe(getURL('/projects'));
 
   const nav = new Navbar(page);
-  const isLoggedIn = await nav.isLoggedIn();
+  await nav.logoutButton.waitFor();
 
+  const isLoggedIn = await nav.isLoggedIn();
   expect(isLoggedIn).toBe(true);
 });
 
@@ -31,8 +31,6 @@ test('cannot login with invalid credentials', async ({ page }) => {
   const badCredentials = createRandomLoginInput();
 
   await pom.login(badCredentials.email, badCredentials.password);
-
-  await page.waitForLoadState('networkidle');
   await pom.error.waitFor();
 
   expect(page.url()).toBe(getURL('/login'));
